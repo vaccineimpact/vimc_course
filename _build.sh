8aa0f15839e74a5a012147cf3a40613c248a8a6b
TJ McKinley <t.mckinley@exeter.ac.uk>
2022-09-07 13:41:11 +0100

    Added chapters 2 and 3, and made various amends to catalytic model. Updated index.Rmd.

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
 ## copy nojekyll file and data
 cp .nojekyll docs
+cp -r data docs
